1f92ceab041ed7df2b5aebd1149700d885dc20f5
GitHub <noreply@github.com>
2023-09-27 17:14:07 +0000

    Update dependencies

--- deps.js
+++ deps.js
@@ -1,6 +1,6 @@
-import { serve } from 'https://deno.land/std@0.202.0/http/server.ts';
+import { serve } from 'https://deno.land/std@0.203.0/http/server.ts';
 import { qrcode } from 'https://deno.land/x/qrcode@v2.0.0/mod.ts';
 
-import { assertEquals } from 'https://deno.land/std@0.202.0/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.203.0/testing/asserts.ts';
 
 export { assertEquals, qrcode, serve };
